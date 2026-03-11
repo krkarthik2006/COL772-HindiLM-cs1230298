7120d3d362957cb8b8a79cf7bf6e619a2a3c90e2
KARTHIK MANIKANDAN RAVIKUMAR <149307601+BEAST-33@users.noreply.github.com>
2026-03-11 21:09:03 +0530

    training loop implemented with minor tweaks req

--- partb/bpe_tokenizer.py
+++ partb/bpe_tokenizer.py
@@ -1,9 +1,89 @@
 class BPETokenizer:
     def __init__(self, vocab_size, special_tokens=None):
-        raise NotImplementedError("BPETokenizer initialization not implemented yet.")
+        self.special_tokens = ['<PAD>','<UNK>','<SOS>','<EOS>'] if special_tokens is None else special_tokens
+        self.vocab_size = vocab_size
+        self.token_to_id = {}
+        self.id_to_token = {}
+        self.merged_tokens = {}
+        self.merged_tokens_rank = {}
+        
 
     def train(self, corpus):
-        raise NotImplementedError("Training method not implemented yet.")
+        #1 create vocab from corpus
+        vocab_word_freq = {}
+        for text in corpus:
+            new_text = text.replace(' ', ' \u0120')
+            for word in new_text.split():
+                if word not in vocab_word_freq:
+                    vocab_word_freq[word] = 0
+                vocab_word_freq[word] += 1
+        #2 initialize token_to_id and id_to_token with special tokens
+        index =0
+        for idx, token in enumerate(self.special_tokens):
+            self.token_to_id[token] = idx
+            self.id_to_token[idx] = token
+            index += 1
+        
+        #3 initialize token_to_id and id_to_token with characters in vocab
+        for word in vocab_word_freq.keys():
+            for char in word:
+                if char not in self.token_to_id:
+                    self.token_to_id[char] = index
+                    self.id_to_token[index] = char
+                    index += 1
+        word_split = {word: list(word) for word in vocab_word_freq.keys()}
+        #4 freq for bpe
+        vocab_token_freq = {}
+        for word, freq in vocab_word_freq.items():
+            tokens = list(word)
+            for i in range(len(tokens)-1):
+                pair = (tokens[i], tokens[i+1])
+                if pair not in vocab_token_freq:
+                    vocab_token_freq[pair] = 0
+                vocab_token_freq[pair] += freq
+        
+        #5 bpe merge
+        rank = 0
+        while (len(self.token_to_id) < self.vocab_size):
+            if not vocab_token_freq:
+                break
+            # find the most frequent pair breaking ties by lexicographical order
+            most_freq_pair = min(vocab_token_freq, key=lambda x: (-vocab_token_freq[x], x))
+            new_token = ''.join(most_freq_pair)
+            self.token_to_id[new_token] = index
+            self.id_to_token[index] = new_token
+            index += 1
+            self.merged_tokens[new_token] = most_freq_pair
+            self.merged_tokens_rank[new_token] = rank
+            rank += 1
+            #update word_split and vocab_token_freq
+            for word, tokens in word_split.items():
+                i = 0
+                new_tokens=[]
+                while i < len(tokens):
+                    if i < len(tokens) - 1:
+                        pair = (tokens[i], tokens[i+1])
+                        if pair == most_freq_pair:
+                            new_tokens.append(new_token)
+                            i += 2
+                        else:
+                            new_tokens.append(tokens[i])
+                            i += 1
+                    else:
+                        new_tokens.append(tokens[i])
+                        i += 1
+                word_split[word] = new_tokens
+            
+            vocab_token_freq = {}
+            for word, freq in vocab_word_freq.items():
+                tokens = word_split[word]
+                for i in range(len(tokens)-1):
+                    pair = (tokens[i], tokens[i+1])
+                    if pair not in vocab_token_freq:
+                        vocab_token_freq[pair] = 0
+                    vocab_token_freq[pair] += freq
+            
+        
     
     def encode(self, text):
         raise NotImplementedError("Encoding method not implemented yet.")
